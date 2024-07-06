834b319eb7956b7a249836bd637bef8b5ac578b7
DanielPaes <daniel.p.aes@hotmail.com>
2024-07-06 19:38:31 -0300

    Implementado as entradas de dados e saída com mensagem de criação da conta.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Informe o número da conta:");
+        Integer conta = scanner.nextInt();
+
+        System.out.println("Informe a agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Informe o nome do cliente:");
+        String nome_cliente = scanner.next();
+
+        System.out.println("Informe o saldo:");
+        Double saldo = scanner.nextDouble();
+
+        System.out.printf("Olá " + nome_cliente + ", obrigado por criar uma conta no nosso banco, sua agência é " + agencia + ", conta " + conta  +" e seu saldo " + saldo + " já está disponível para saque");
+
+        scanner.close();
     }
 }
