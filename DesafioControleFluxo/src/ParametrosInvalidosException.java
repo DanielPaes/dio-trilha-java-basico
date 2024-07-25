6f67a3b23ebeb8567c4b1075c3636045dc3f8e1f
DanielPaes <daniel.p.aes@hotmail.com>
2024-07-25 13:29:44 -0300

    Correção controle de fluxo.

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++
@@ -1,6 +0,0 @@
-public class ParametrosInvalidosException extends Exception{
-    @Override
-    public String getMessage(){
-        return "O segundo parâmetro deve ser maior que o primeiro.";
-    }
-}
